e1cd1d91dfbc39132f8b0dd4d928b5d6295f0d62
sh1mj1 <7wlgns@gmail.com>
2022-10-09 15:23:36 +0900

    request scope bean is DI (using Provider)

--- src/main/java/com/example/helloOnlyJava/core/web/LogDemoController.java
+++ src/main/java/com/example/helloOnlyJava/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package com.example.helloOnlyJava.core.web;
 
 import com.example.helloOnlyJava.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,18 +14,32 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor // 자동 생성자 주입
 public class LogDemoController {
     public final LogDemoService logDemoService;
-    public final MyLogger myLogger;
+    //    public final MyLogger myLogger;
+    public final ObjectProvider<MyLogger> myLoggerProvider; // myLogger 을 찾을 수 있는 ObjectProvider 주입
 
     // log- demo 요청이 오면 웹 브라우저에 데이터를 문자로 변환
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
-        // HttpServletRequest: 클라이언트 요청 정봏
-        String requestURL = request.getRequestURL().toString(); // 클라이언트가 요청한 URL 반환
-        myLogger.setRequestURL(requestURL); // myLogger 의 requestURL 에 값 저장
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        String requestURL = request.getRequestURL().toString();
+        // ObjectProvider 로 myLogger 조회 (DL)
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL); // myLogger 에 requestURL 저장
 
+//        myLogger.log("LogDemoController-MyLogger: " + myLogger);
         myLogger.log("controller test");
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "OK";
+
     }
+//    public String logDemo(HttpServletRequest request) {
+//        // HttpServletRequest: 클라이언트 요청 정봏
+//        String requestURL = request.getRequestURL().toString(); // 클라이언트가 요청한 URL 반환
+//        myLogger.setRequestURL(requestURL); // myLogger 의 requestURL 에 값 저장
+//
+//        myLogger.log("controller test");
+//        logDemoService.logic("testId");
+//        return "OK";
+//    }
 }
--- src/main/java/com/example/helloOnlyJava/core/web/LogDemoService.java
+++ src/main/java/com/example/helloOnlyJava/core/web/LogDemoService.java
@@ -2,14 +2,20 @@ package com.example.helloOnlyJava.core.web;
 
 import com.example.helloOnlyJava.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final MyLogger myLogger;
+    //    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;  // myLogger 을 찾을 수 있는 ObjectProvider 주입
 
     public void logic(String id) {
+//        myLogger.log("service id = " + id);
+        // DL (ObjectProvider 로 myLogger 조회 (DL)
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.log("service id = " + id);
+//        myLogger.log("LogDemoService-MyLogger : " + myLogger);
     }
 }
